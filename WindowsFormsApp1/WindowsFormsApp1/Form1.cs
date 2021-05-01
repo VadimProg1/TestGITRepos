ad15dd24a9ea6d919d2c6b4d7a8f7ea6f1921bc1
Вадим Бабиков <b.vadim.alexeyevich@gmail.com>
2021-05-01 09:18:11 +0700

    added SomeFunc

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,15 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public int SomeFunc()
+        {
+            return 0;
         }
     }
 }
